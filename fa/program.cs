67f3378ef0ed0fdcc711f7f416d3e8bf0a15783f
qutyxd <100722945+qutyxd@users.noreply.github.com>
2022-03-26 15:20:54 +0300

    Update program.cs

--- fa/program.cs
+++ fa/program.cs
@@ -32,7 +32,7 @@ namespace fans
     {
       Name = "c",
       Transitions = new Dictionary<char, State>(),
-      IsAcceptState = true,
+      IsAcceptState = false,
     };
      public static State d = new State()
     {
@@ -44,31 +44,28 @@ namespace fans
     {
       Name = "e",
       Transitions = new Dictionary<char, State>(),
-      IsAcceptState = false,
+      IsAcceptState = true,
     };
     State InitialState = a;
     public FA1()
     {
       a.Transitions['0'] = b;
-      a.Transitions['1'] = d;
-      b.Transitions['1'] = c;
-      b.Transitions['0'] = e;
+      a.Transitions['1'] = c;
+      b.Transitions['1'] = e;
+      b.Transitions['0'] = d;
       c.Transitions['0'] = e;
       c.Transitions['1'] = c;
-      d.Transitions['0'] = c;
+      d.Transitions['0'] = d;
       d.Transitions['1'] = d;
-      e.Transitions['0'] = e;
+      e.Transitions['0'] = d;
       e.Transitions['1'] = e;
     }
     public bool? Run(IEnumerable<char> s)
     {
-      State current = a;
+      State current = InitialState;
       foreach (var c in s)
       {
-        Console.WriteLine(current.Name);
         current = current.Transitions[c];
-        if (current == e)
-        {return false;}
         if (current == null)
           return null;   
       }
@@ -88,7 +85,7 @@ namespace fans
     {
       Name = "b",
       Transitions = new Dictionary<char, State>(),
-      IsAcceptState = true,
+      IsAcceptState = false,
     };
     public static State c = new State()
     {
@@ -100,25 +97,25 @@ namespace fans
     {
       Name = "d",
       Transitions = new Dictionary<char, State>(),
-      IsAcceptState = false,
+      IsAcceptState = true,
     };
+    State InitialState = a;
     public FA2()
     {
-      a.Transitions['0'] = d;
-      a.Transitions['1'] = b;
-      b.Transitions['1'] = a;
-      b.Transitions['0'] = c;
-      c.Transitions['0'] = b;
-      c.Transitions['1'] = d;
-      d.Transitions['0'] = a;
-      d.Transitions['1'] = c;
+      a.Transitions['0'] = b;
+      a.Transitions['1'] = c;
+      b.Transitions['1'] = d;
+      b.Transitions['0'] = a;
+      c.Transitions['0'] = d;
+      c.Transitions['1'] = a;
+      d.Transitions['0'] = c;
+      d.Transitions['1'] = b;
     }
     public bool? Run(IEnumerable<char> s)
     {
-      State current = b;
+      State current = InitialState;
       foreach (var c in s)
       {
-        Console.WriteLine(current.Name);
         current = current.Transitions[c];
         if (current == null)
           return null;
@@ -126,18 +123,73 @@ namespace fans
       return current.IsAcceptState;
     }
   }
-  
+  public class FA3
+  {
+    public static State a = new State()
+    {
+      Name = "a",
+      Transitions = new Dictionary<char, State>(),
+      IsAcceptState = false
+    };
+    public static State b = new State()
+    {
+      Name = "b",
+      Transitions = new Dictionary<char, State>(),
+      IsAcceptState = false
+    };
+    public static State c = new State()
+    {
+      Name = "c",
+      Transitions = new Dictionary<char, State>(),
+      IsAcceptState = false
+    };
+    public State d = new State()
+    {
+      Name = "d",
+      Transitions = new Dictionary<char, State>(),
+      IsAcceptState = true
+    };
+    State InitialState = a;
+    public FA3()
+    {
+      a.Transitions['0'] = b;
+      a.Transitions['1'] = c;
+      b.Transitions['0'] = a;
+      b.Transitions['1'] = c;
+      c.Transitions['0'] = a;
+      c.Transitions['1'] = d;
+      d.Transitions['0'] = d;
+      d.Transitions['1'] = d;
+    }
+    public bool? Run(IEnumerable<char> s)
+    {
+      State current = InitialState;
+      foreach (var c in s)
+      {
+        current = current.Transitions[c];
+        if (current == null)
+          return null;
+      }
+      return current.IsAcceptState;
+    }
+  }
+    
   class Program
   {
     static void Main(string[] args)
     {
-      String s = "01111";
+      String s1 = "1111011111";
+      String s2 = "0010010111";
+      String s3 = "0000010111";
       FA1 fa1 = new FA1();
-      bool? result1 = fa1.Run(s);
+      bool? result1 = fa1.Run(s1);
       Console.WriteLine(result1);
       FA2 fa2 = new FA2();
-      bool? result2 = fa2.Run(s);
+      bool? result2 = fa2.Run(s2);
       Console.WriteLine(result2);
+      FA3 fa3 = new FA3();
+      bool? result3 = fa3.Run(s3);
+      Console.WriteLine(result3);
     }
   }
 }
